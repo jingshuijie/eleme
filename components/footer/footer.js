8c95436c4427289ed9ae4855177db6c63f30c549
Wang jz <1191188265@qq.com>
2019-03-04 20:48:58 +0800

    bugfix

---
+++ components/footer/footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+class Footer extends React.Component{
+    render(){
+        return (<footer>
+            订单
+        </footer>)
+    }
+}
+export default Footer; 

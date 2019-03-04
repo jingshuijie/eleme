b9c883abbd209edbebe4a234586b39e158909649
mygit <674478687@qq.com>
2019-03-04 20:29:34 +0800

    init5

---
+++ components/App.js
@@ -0,0 +1,14 @@
+﻿import React from 'react';
+import Child from './Child'
+
+class App extends React.Component{
+render(){
+		return(
+			<div>
+				<h1>webpack 构建react开发环境</h1>
+			</div>
+		)
+	}
+}
+
+export default App;

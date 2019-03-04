c839eeaa8e1225186c12bb3211cdb867649c2deb
xiaobai <550726844@qq.com>
2019-03-04 20:00:08 +0800

    -yuan1

---
+++ components/section/section.js
@@ -0,0 +1,11 @@
+import React from "react";
+improt ReactDOM from "react-dom";
+class section extends React.Component{
+render(){
+return <div>
+<section></section>
+</div>
+}
+
+}
+export default section

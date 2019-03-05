c5a6072a2d2c219c47d821e8a1182eae672c04b3
mygit <674478687@qq.com>
2019-03-05 08:09:08 +0800

    finish1

--- components/App.js
+++ components/App.js
@@ -1,11 +1,14 @@
 ﻿import React from 'react';
-import Child from './Child'
-
+import Footer from "./footer/footer.js";
+import Header from "./header/header.js";
+import Section from "./section/section.js";
 class App extends React.Component{
 render(){
 		return(
-			<div>
-				<h1>webpack 构建react开发环境</h1>
+			<div id="inner">
+				<Header/>
+				<Section/>
+				<Footer/>
 			</div>
 		)
 	}
--- main.js
+++ main.js
@@ -1,8 +1,5 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import './css/stylesheet.css'
-ReactDOM.render(<App/>,document.getElementById('out'))
-
-
+import "./css/style.css";
+ReactDOM.render( < App / > , document.getElementById('out'))
